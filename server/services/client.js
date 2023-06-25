bbb95bce3c0dfc3fbee404eeab6a6e46c2ba0803
Mayoreta <loviana.mayoreta@alteacare.com>
2023-06-25 15:02:17 +0700

    fix: fix timeout variable

--- server/services/client.js
+++ server/services/client.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = ({ strapi }) => ({
   init() {
@@ -7,72 +7,76 @@ module.exports = ({ strapi }) => ({
     const setRedisData = async ({ key, data, expiredInSeconds = 0 }) => {
       const newKey = `${strapi.redis.prefix}:${key}`;
       if (expiredInSeconds > 0) {
-        return redis.setex(newKey, timeout, data);
+        return redis.setex(newKey, expiredInSeconds, data);
       }
       return redis.set(newKey, data);
     };
-    
-    const getRedisData = async (key) => new Promise((resolve, reject) => {
-      const newKey = `${strapi.redis.prefix}:${key}`;
-      redis.get(newKey, (err, data) => {
-        if (err) {
-          reject(err);
-          return;
-        }
 
-        resolve(data);
-      })
-    });
-    
+    const getRedisData = async (key) =>
+      new Promise((resolve, reject) => {
+        const newKey = `${strapi.redis.prefix}:${key}`;
+        redis.get(newKey, (err, data) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          resolve(data);
+        });
+      });
+
     const deleteRedisData = async (key) => {
       const newKey = `${strapi.redis.prefix}:${key}`;
-      return redis.del(newKey)
+      return redis.del(newKey);
     };
-    
-    const increment = async ({ key, expiredInSeconds = 0 }) => new Promise((resolve, reject) => {
-      const newKey = `${strapi.redis.prefix}:${key}`;
-    
-      redis.incr(newKey, (err, data) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        if (expiredInSeconds > 0) {
-          redis.expire(newKey, expiredInSeconds);
-        }
 
-        resolve(data);
-      })
-    });
-    
-    const getTimeoutIncrementKey = async (key) => new Promise((resolve, reject) => {
-      const newKey = `${strapi.redis.prefix}:${key}`;
-      redis.ttl(newKey, (err, data) => {
-        if (err) {
-          reject(err);
-          return;
-        }
+    const increment = async ({ key, expiredInSeconds = 0 }) =>
+      new Promise((resolve, reject) => {
+        const newKey = `${strapi.redis.prefix}:${key}`;
 
-        resolve(data);
-      })
-    });
-    
-    const getUniqueProcess = async ({ key, expiredInSeconds = 0 }) => new Promise((resolve, reject) => {
-      const newKey = `${strapi.redis.prefix}:${key}`;
-    
-      redis.incr(newKey, (err, data) => {
-        if (err) {
-          reject(err);
-          return;
-        }
+        redis.incr(newKey, (err, data) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          if (expiredInSeconds > 0) {
+            redis.expire(newKey, expiredInSeconds);
+          }
 
-        if (data === 1 && expiredInSeconds > 0) {
-          redis.expire(newKey, expiredInSeconds);
-        }
-
-        resolve(data === 1);
+          resolve(data);
+        });
       });
-    });
+
+    const getTimeoutIncrementKey = async (key) =>
+      new Promise((resolve, reject) => {
+        const newKey = `${strapi.redis.prefix}:${key}`;
+        redis.ttl(newKey, (err, data) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          resolve(data);
+        });
+      });
+
+    const getUniqueProcess = async ({ key, expiredInSeconds = 0 }) =>
+      new Promise((resolve, reject) => {
+        const newKey = `${strapi.redis.prefix}:${key}`;
+
+        redis.incr(newKey, (err, data) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          if (data === 1 && expiredInSeconds > 0) {
+            redis.expire(newKey, expiredInSeconds);
+          }
+
+          resolve(data === 1);
+        });
+      });
 
     strapi.redis.client = {
       set: setRedisData,
